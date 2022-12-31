dcdb3981654a0391bb53a254ee2bbf2dd88edf95
guavalines <konstrictor@gmail.com>
2022-12-30 18:22:16 -0600

    added countdown function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const score = document.querySelector('#score')
 
 let result = 0
 let hitPosition
+let currentTime = 60
 
 function randomSquare() {
   squares.forEach(square => {
@@ -18,10 +19,9 @@ function randomSquare() {
 }
 
 squares.forEach(square => {
-  square.addEventListener('mousedown' () => {
+  square.addEventListener('mousedown', () => {
     if (square.id == hitPosition) {
       result++
-      console.log(result)
       score.textContent = result
       hitPosition = null
     }
@@ -30,7 +30,20 @@ squares.forEach(square => {
 
 function moveMole() {
   let timerId = null
-  timerId = setInterval(randomSquare, 500)
+  timerId = setInterval(randomSquare, 1000)
 }
 
 moveMole()
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId)
+    alert('GAME OVER! Your final score is' + result)
+  }
+
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
